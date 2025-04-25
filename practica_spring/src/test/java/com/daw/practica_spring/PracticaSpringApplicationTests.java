3256c8db9bcfbf2a417a3366413c194dbe9dd591
Juanpa-jpg <juanpanoy13@comunidad.unam.mx>
2025-04-24 21:07:21 -0600

    Initial commit

---
+++ practica_spring/src/test/java/com/daw/practica_spring/PracticaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.practica_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
